15024321bb84def8f781603a2a873608ba942295
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-11-26 09:32:47 +0530

    modify file to remove swap files to remove swap files repeatedly

--- removeSwapFiles.sh
+++ removeSwapFiles.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-rm .*.swp 2>>/dev/null
+rm -rf .*.swp 2>>/dev/null
 exitvalue=$?
-if [ $exitvalue == 0 ];then
+if [ $exitvalue -eq 0 ];then
   echo "Swap files removed successfully"
 else 
   echo "No swap files found"
